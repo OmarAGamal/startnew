ea6837afa20bf44f7964ae09963bc377cde6953d
Omar Abdelmoneim Gamal <58474037+OmarAGamal@users.noreply.github.com>
2023-05-18 09:50:28 +0300

    Add files via upload

---
+++ src/main/java/com/example/SkarptHub/SkarptHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.SkarptHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkarptHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkarptHubApplication.class, args);
+	}
+
+}
